5bfd4912cb904de3205a69e9bdb68ca0290d8f4f
oliverpople <oliverpople@gmail.com>
2018-01-02 09:51:56 +0000

    selectBook action creator now returns action object; a type property (i.e. a describing its purpose) and its payload (i.e. the data being returned).  Also created active_ _book reducer, imported it into the reducer index.js file and added it to combineReducers.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book) {
-  console.log('A book has been selected:', book.title);
+  // selectBook is an ActionCreator, it needs to return an action,
+  // an object with a type property (i.e. the action's purpose)
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
