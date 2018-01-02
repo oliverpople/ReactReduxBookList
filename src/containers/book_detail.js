acea4d65b2dabc7afd62e3c5ddb9240610db2dd1
oliverpople <oliverpople@gmail.com>
2018-01-02 11:01:57 +0000

    Added check to the render method of component BookDetail. This check prevents component from calling a property within active_book reducer/’s whilst its default state =  null.

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -3,9 +3,16 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
-      return (
-        <div>Book Detail!</div>
-      );
+    if (!this.props.book) {
+      return <div>Select a book to get started.</div>;
+    }
+
+    return (
+      <div>
+        <h3> Details for:</h3>
+        <div>{this.props.book.title}</div>
+      </div>
+    );
   }
 }
 
